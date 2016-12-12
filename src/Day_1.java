4c6f3e69a562caaeb9df87d9df8ce46c95297c63
Todd Griffin <nemagnusfrater@gmail.com>
2016-12-11 23:06:41 -0500

    Rewrote Day 1 code using line segments/vectors.

--- src/Day_1.java
+++ src/Day_1.java
@@ -1,22 +1,76 @@
 import tools.*;
 
+import java.util.ArrayList;
+
 /**
  * Created by magnusfrater on 12/10/16.
  */
 public class Day_1 {
 
+    private ArrayList<Vector> directions = new ArrayList<>();
+
+    public int getDistanceToHQ () {
+        String[] input = Utils.parseFile("DirectionsToBunnyHQ.txt").get(0).split(", ");
+
+        directions = new ArrayList<>();
+
+        directions.add(new Vector(new Position(0,0), 0, 90));
+
+        for (String s : input) {
+            Vector lastVector = directions.get(directions.size() - 1);
+            double degrees = lastVector.getDegrees();
+
+            if (s.contains("L")) {
+                degrees += 90;
+            } else {
+                degrees -= 90;
+            }
+
+            Vector nextVector = new Vector(lastVector.getEnd(), Integer.parseInt(s.substring(1)), degrees);
+            directions.add(nextVector);
+        }
+
+        Vector finalVector = directions.get(directions.size() - 1);
+
+        return Math.abs(finalVector.getEnd().getX()) + Math.abs(finalVector.getEnd().getY());
+    }
+
+    public int getFirstIntersection () {
+        // can't intersect with the line before it
+        for (int i=0; i<directions.size(); i++) {
+            for (int j=0; j<i - 1; j++) {
+                // if the lines intersect
+                if (directions.get(i).doesIntersect(directions.get(j))) {
+
+                    // get the exact point of the intersection, recreate that intersected line using that point as the end
+                    Position intersection = directions.get(i).getIntersection(directions.get(j));
+                    Vector intersected = new Vector(directions.get(j).getStart(), intersection);
+
+                    // get the total x & y offset (to total the blocks)
+                    double xOffset = Math.abs(intersected.getStart().getX());
+                    double yOffset = Math.abs(intersected.getStart().getY());
+
+                    Vector v1 = directions.get(i);
+                    Vector v2 = directions.get(j);
+
+                    System.out.println(i +" "+ j);
+                    System.out.println(v1.getStart().getX() +" "+ v1.getStart().getY() +" "+ v1.getEnd().getX() +" "+ v1.getEnd().getY());
+                    System.out.println(v2.getStart().getX() +" "+ v2.getStart().getY() +" "+ v2.getEnd().getX() +" "+ v2.getEnd().getY());
+                    System.out.println(xOffset +" "+ yOffset);
+
+                    return (int)(xOffset + yOffset);
+                }
+            }
+        }
+
+        return -1;
+    }
+
     public static void main (String[] args) {
         Day_1 d1 = new Day_1();
 
-        int bunnyHQ = 0;
-        int firstIntersection = 0;
-
-        Vector v1 = new Vector(new Position(0,0), 3, 90);
-        Vector v2 = new Vector(new Position(-1,1), 3, 0);
-
-        Position intersection = v1.intersects(v2);
-
-        System.out.println(intersection.getX() +" "+ intersection.getY());
+        int bunnyHQ = d1.getDistanceToHQ();
+        int firstIntersection = d1.getFirstIntersection();
 
         System.out.println("The distance to the Easter Bunny's HQ is "+ bunnyHQ +" blocks.");
         System.out.println("The first position we visit twice is "+ firstIntersection +" blocks away.");
--- src/tools/Vector.java
+++ src/tools/Vector.java
@@ -1,5 +1,7 @@
 package tools;
 
+import java.awt.geom.Line2D;
+
 /**
  * Created by magnusfrater on 12/10/16.
  */
@@ -14,6 +16,23 @@ public class Vector {
         this.start = start;
         this.magnitude = magnitude;
         this.degrees = degrees;
+
+        fixDegrees();
+    }
+
+    public Vector (Position start, Position end) {
+        this.start = start;
+
+        // if a vertical line
+        if (end.getX() == start.getX()) {
+            this.magnitude = Math.abs(end.getY() - start.getY());
+        } else {
+            this.magnitude = Math.abs(end.getY() - start.getY()) / Math.abs(end.getX() - start.getX());
+        }
+
+        this.degrees = Math.atan2(end.getY() - start.getY(), end.getX() - start.getX());
+
+        fixDegrees();
     }
 
     public Position getStart() {
@@ -38,6 +57,19 @@ public class Vector {
 
     public void setDegrees(double degrees) {
         this.degrees = degrees;
+
+        fixDegrees();
+    }
+
+    // -360 <= degrees <= 360
+    public void fixDegrees () {
+        if (degrees > 360) {
+            degrees -= 360;
+        }
+
+        if (degrees < -360) {
+            degrees += 360;
+        }
     }
 
     public Position getEnd () {
@@ -51,7 +83,43 @@ public class Vector {
         return end;
     }
 
-    public Position intersects (Vector vector) {
+    // returns a double length difference between end.x and start.x
+    public double getXOffset () {
+        return getEnd().getX() - start.getX();
+    }
 
+    // returns a double length difference between end.y and start.y
+    public double getYOffset () {
+        return getEnd().getY() - start.getY();
+    }
+
+    public Position getIntersection (Vector vector) {
+
+        double x1 = start.getX();
+        double y1 = start.getY();
+        double x2 = getEnd().getX();
+        double y2 = getEnd().getY();
+
+        double x3 = vector.getStart().getX();
+        double y3 = vector.getStart().getY();
+        double x4 = vector.getEnd().getX();
+        double y4 = vector.getEnd().getY();
+
+        double denom = (y4 - y3) * (x2 - x1) - (x4 - x3) * (y2 - y1);
+        if (denom == 0.0) { // Lines are parallel.
+            return null;
+        }
+        double ua = ((x4 - x3) * (y1 - y3) - (y4 - y3) * (x1 - x3)) / denom;
+        double ub = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / denom;
+        if (ua >= 0.0f && ua <= 1.0f && ub >= 0.0f && ub <= 1.0f) {
+            // Get the intersection point.
+            return new Position((x1 + ua * (x2 - x1)), (int) (y1 + ua * (y2 - y1)));
+        }
+
+        return null;
+    }
+
+    public boolean doesIntersect (Vector vector) {
+        return Line2D.linesIntersect(getStart().getX(), getStart().getY(), getEnd().getX(), getEnd().getY(), vector.getStart().getX(), vector.getStart().getY(), vector.getEnd().getX(), vector.getEnd().getY());
     }
 }
